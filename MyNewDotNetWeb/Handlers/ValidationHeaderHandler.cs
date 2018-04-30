45b23c22098b55508e5d09c135517da6cd6c1723
NgelJoseph <angel.josephit@gmail.com>
2018-04-30 16:40:17 +1000

    Adding Validation and timing handler

---
+++ MyNewDotNetWeb/Handlers/ValidationHeaderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyNewDotNetWeb.Handlers
+{
+    public class ValidationHeaderHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("X-Api-Key"))
+            {
+                return new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent("You must supply API key X-Api-Key")
+                };
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        } 
+    }
+}
